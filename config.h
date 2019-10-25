d53ed00427a15016d666eb3365f2d015c812b658
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2019-10-25 21:37:22 +0300

    added systray patch

--- config.h
+++ config.h
@@ -2,6 +2,10 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;     /* 0 means no systray */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 12;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
