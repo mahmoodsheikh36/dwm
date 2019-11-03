93bb0e4bcdbf418eb75b8f6a9bb403e5531e0d20
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2019-11-03 18:47:15 +0200

    back to arch

--- config.h
+++ config.h
@@ -12,7 +12,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const double defaultopacity  = 1;
-static const char *fonts[]          = { "Ubuntu:size=11" };
+static const char *fonts[]          = { "Inconsolata:size=11" };
 static const char dmenufont[]       = "MerriweatherSans:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -98,12 +98,6 @@ static const char *playprev[] =    { "playerctl", "previous", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-    /* { 0,         XF86XK_AudioLowerVolume,      spawn,          {.v = downvol } }, */
-    /* { 0,         XF86XK_AudioRaiseVolume,      spawn,          {.v = upvol } }, */
-    /* { 0,                XF86XK_AudioPrev,      spawn,          {.v = playprev } }, */
-    /* { 0,                XF86XK_AudioNext,      spawn,          {.v = playnext } }, */
-    /* { 0,                XF86XK_AudioPlay,      spawn,          {.v = toggletrack } }, */
-	/* { MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } }, */
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_x,      spawn,          {.v = dmenuScriptCmd } },
  	/* { MODKEY|ShiftMask,		        XK_s,	   spawn,	   SHCMD("transset-df -a --dec .1") }, */
