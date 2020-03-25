71efc4f912185dafd548147e83a2fdecf1b5c13a
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-03-25 02:33:10 +0200

    increase font size

--- config.h
+++ config.h
@@ -12,7 +12,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const double defaultopacity  = 1;
-static const char *fonts[]          = { "Inconsolata:size=12" };
+static const char *fonts[]          = { "Inconsolata:size=13" };
 static const char dmenufont[]       = "MerriweatherSans:size=11";
 // static const char col_gray1[]       = "#222222";
 // static const char col_gray2[]       = "#444444";
